505a252b5f94891be58f3b445a0bf1592c337637
Luan Milagre <luanmilagrereal@gmail.com>
2026-09-15 17:18:40 -0400

    Projeto de tipos de dados em Java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        byte idade =25; //8 bits
+        short ano = 2024; //16 bits
+        int populacaoCidade = 50000; //32 bits
+        long populacaoMundial = 7800000000L; //64bits
+        float altura = 1.75f; //32 bits
+        double salario = 4500.50; //64 bits
+        boolean estudante = true; 
+        char inicialNome = 'L'; //16 bits
+
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("Populacao da Cidade: " + populacaoCidade);
+        System.out.println("População Mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salario: " + salario);
+        System.out.println("É estudante? " + estudante);
+        System.out.println("Letra inical do nome: " + inicialNome);
+    }
+}   
